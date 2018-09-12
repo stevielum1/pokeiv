4f10651c5b9c5cf1586e379fc2167d901a435586
Steven Lum <stevielum1@gmail.com>
2018-09-11 21:43:50 -0700

    Add boilerplate for react/redux

---
+++ frontend/store/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const configureStore = () => createStore(
+  rootReducer,
+  {},
+  applyMiddleware(thunk, logger)
+);
+
+export default configureStore;
