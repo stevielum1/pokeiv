5e268c0bd3ff387c7c874f21c2df75b987d0f606
Steven Lum <stevielum1@gmail.com>
2018-09-13 22:10:23 -0700

    Add more seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,32 @@ end
 createPokemon("Bulbasaur", 90, 118, 118)
 createPokemon("Ivysaur", 120, 151, 151)
 createPokemon("Venusaur", 160, 198, 198)
+
+createPokemon("Charmander", 78, 116, 96)
+createPokemon("Charmeleon", 116, 158, 129)
+createPokemon("Charizard", 156, 223, 176)
+
+createPokemon("Squirtle", 88, 94, 122)
+createPokemon("Wartortle", 118, 126, 155)
+createPokemon("Blastoise", 158, 171, 210)
+
+createPokemon("Caterpie", 90, 55, 62)
+createPokemon("Metapod", 100, 45, 94)
+createPokemon("Butterfree", 120, 167, 151)
+
+createPokemon("Weedle", 80, 63, 55)
+createPokemon("Kakuna", 90, 46, 86)
+createPokemon("Beedril", 130, 169, 150)
+
+createPokemon("Pidgey", 80, 85, 76)
+createPokemon("Pidgeotto", 126, 117, 108)
+createPokemon("Pidgeot", 166, 166, 157)
+
+createPokemon("Rattata", 60, 103, 70)
+createPokemon("Raticate", 110, 161, 144)
+
+createPokemon("Spearow", 80, 112, 61)
+createPokemon("Fearow", 130, 182, 135)
+
+createPokemon("Ekans", 70, 110, 102)
+createPokemon("Arbok", 120, 167, 158)
